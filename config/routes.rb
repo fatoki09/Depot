2b00ce7b9651a34ef9bd040b86cc7a57df2e6fd8
Raleigh Fatoki <fatoki09@yahoo.com>
2013-08-09 14:51:37 -0500

    First commit. MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,32 @@
 Depot::Application.routes.draw do
-  resources :products
+ get 'admin' => 'admin#index'
+ controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :users
+
+  get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+  resources :orders
+  resources :line_items
+  resources :carts
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'store#index', as: 'store'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +76,5 @@ Depot::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+end
 end
